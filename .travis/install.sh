264df8bf37d946ea70e749730063d27bd865a220
Marina Gourtovaia <mg8@sanger.ac.uk>
2019-08-20 12:35:24 +0100

    pin the build to Ubuntu precise; build npg Perl packages the same way as in other Travis CI build for npg packages

--- .travis/install.sh
+++ .travis/install.sh
@@ -138,25 +138,28 @@ popd
 
 # Third party tools install done
 
-# CPAN as in npg_npg_deploy
-cpanm --notest --reinstall App::cpanminus
-cpanm --no-lwp --notest https://github.com/wtsi-npg/perl-dnap-utilities/releases/download/${DNAP_UTILITIES_VERSION}/WTSI-DNAP-Utilities-${DNAP_UTILITIES_VERSION}.tar.gz
+sudo apt-get install libgd2-xpm-dev # for npg_tracking
+cpanm --quiet --notest LWP::Protocol::https
 
+# Git branch to merge to or custom branch
+WTSI_NPG_BUILD_BRANCH=${WTSI_NPG_BUILD_BRANCH:=$TRAVIS_BRANCH}
 # WTSI NPG Perl repo dependencies
-cd /tmp
-git clone --branch devel --depth 1 https://github.com/wtsi-npg/ml_warehouse.git ml_warehouse.git
-git clone --branch devel --depth 1 https://github.com/wtsi-npg/npg_tracking.git npg_tracking.git
-
-repos="/tmp/ml_warehouse.git /tmp/npg_tracking.git"
+repos="perl-dnap-utilities ml_warehouse npg_tracking"
 
 for repo in $repos
 do
-      cd "$repo"
-        cpanm --quiet --notest --installdeps . || find /home/travis/.cpanm/work -cmin -1 -name '*.log' -exec tail -n20  {} \;
-          perl Build.PL
-            ./Build
-            ./Build install
-          done
+  cd /tmp
+  # Always clone master when using depth 1 to get current tag
+  git clone --branch master --depth 1 ${WTSI_NPG_GITHUB_URL}/${repo}.git ${repo}.git
+  cd /tmp/${repo}.git
+  # Shift off master to appropriate branch (if possible)
+  git ls-remote --heads --exit-code origin ${WTSI_NPG_BUILD_BRANCH} && git pull origin ${WTSI_NPG_BUILD_BRANCH} && echo "Switched to branch ${WTSI_NPG_BUILD_BRANCH}"
+  cpanm --quiet --notest --installdeps . || find /home/travis/.cpanm/work -cmin -1 -name '*.log' -exec tail -n20  {} \;
+  perl Build.PL
+  ./Build
+  ./Build install
 
-          cd "$TRAVIS_BUILD_DIR"
+done
+
+cd "$TRAVIS_BUILD_DIR"
 
